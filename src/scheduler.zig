2c519e04e04fa048531d4c77d568c7ad2f9d1995
djames-bloom <dan@bloommoney.co>
2026-02-03 12:11:09 +0000

    init scheduler

--- src/scheduler.zig
+++ src/scheduler.zig
@@ -47,3 +47,43 @@ pub const CheckpointPayload = struct {
 pub const InjectedFaultPayload = struct {
     target_id: u32,
 };
+
+pub const Scheduler = struct {
+    event_queue: std.PriorityQueue(Event, void, eventLessThan),
+    clock: *Clock,
+    rng: *DeterministicRNG,
+    next_event_id: u64,
+    events_processed: u64,
+    max_time: u64,
+
+    pub fn init(
+        allocator: std.mem.Allocator,
+        clock: *Clock,
+        rng: *DeterministicRNG,
+    ) Scheduler {
+        return .{
+            .event_queue = std.PriorityQueue(Event, void, eventLessThan).init(allocator, {}),
+            .clock = clock,
+            .rng = rng,
+            .next_event_id = 0,
+            .events_processed = 0,
+            .max_time = 5_000_000_000, // 5sec of sim time
+        };
+    }
+
+    pub fn deinit(self: *Scheduler) void {
+        self.event_queue.deinit();
+    }
+};
+
+fn eventLessThan(context: void, x: Event, y: Event) std.math.Order {
+    _ = context;
+
+    if (x.scheduled_time < y.scheduled_time) return .lt;
+    if (x.scheduled_time > y.scheduled_time) return .gt;
+
+    if (x.id < y.id) return .lt;
+    if (x.id > y.id) return .gt;
+
+    return .eq;
+}
